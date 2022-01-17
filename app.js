8a9f3515f04e9150226141d2ee4f950abc10960e
perelokesh <lokesh7093@gmail.com>
2022-01-17 16:09:36 +0530

    db

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv')
+
+
+dotenv.config();
+ mongoose.connect(process.env.MONGO_URL, ()=>{
+     console.log('connected to Mongoose server');
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen( port, () => {
