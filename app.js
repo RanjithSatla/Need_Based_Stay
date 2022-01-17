f342a14d5e5cec0896203773e64e0727d055194f
RanjithSatla <ranjithsatla327@gmail.com>
2022-01-17 16:45:19 +0530

    Server

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const db = require("./config/db");
 
 const port = process.env.PORT || 3000;
 
+app.use("/", (req, res) => {
+  res.send("Hello");
+});
+
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
