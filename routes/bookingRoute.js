c050a0554937e2048ffc28e9d341bd5b3d30feb4
RanjithSatla <ranjithsatla327@gmail.com>
2022-02-07 09:48:42 +0530

    C

--- routes/bookingRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const booking = require("../controllers/bookingController");
-
-const router = express.Router();
-
-router.post("/booking", booking.bookProperty);
-
-module.exports = router;
